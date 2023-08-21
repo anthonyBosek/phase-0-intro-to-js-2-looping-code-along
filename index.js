4680b510ebd2321177526b43f20049f8ee5a6977
Anthony Bosek <abosek006@gmail.com>
2023-08-21 13:46:38 -0700

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+  }
+  return messages;
+}
+
+function countDown(int) {
+  if (int < 0) return "Must be a postitve number!";
+
+  while (int >= 0) {
+    console.log(int);
+    int--;
+  }
+}
